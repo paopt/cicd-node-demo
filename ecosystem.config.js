aedb69b3329dda561c9b6ba6dbf6fc10de6ed0ca
yaojinfeng <yaojinfeng0604@hhdd.com>
2024-10-28 14:10:54 +0800

    deploy node

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps : [{
+    script: './src/app.js',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '118.31.167.102',
+      ref  : 'origin/main',
+      repo : 'https://github.com/paopt/cicd-node-demo',
+      path : '/var/www/cicd-node',
+      'pre-deploy-local': '',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
